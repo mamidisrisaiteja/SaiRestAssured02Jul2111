f65abf39eb316f19044d912e315b8401f1118eaa
AN574BV <AN574BV@IN3072595W1.ey.net>
2022-07-03 14:07:49 +0530

    New Commit

---
+++ MvnTest3/src/main/java/MvnTest3/MvnTest3/App.java
@@ -0,0 +1,13 @@
+package MvnTest3.MvnTest3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ MvnTest3/src/main/java/MvnTest3/MvnTest3/Demo4.java
@@ -0,0 +1,10 @@
+package MvnTest3.MvnTest3;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
